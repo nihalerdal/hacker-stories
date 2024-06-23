4f42e03670073d5c654f8a3006cf32e3f87b2425
nihalerdal <nihalerdall@gmail.com>
2024-06-23 12:07:15 -0700

    Refactoring-arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,55 @@
-import * as React from 'react';
+import * as React from "react";
 
-
-const list = [{
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0,
-},
-{
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1,
-}]
+  },
+];
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
 
-      <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' />
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>{" "}
+        <span>{item.author}</span> <span>{item.num_comments}</span>{" "}
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
 
-      <hr />
-      {/*render the list */}
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-   {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              {' '}<span>{item.author}</span>
-              {' '}<span>{item.num_comments}</span>
-              {' '}<span>{item.points}</span>
-            </li> );
-          })}
-    </div> 
-  );
-}
+    <Search />
+
+    <hr />
+
+    <List />
+  </div>
+);
 
 export default App;
