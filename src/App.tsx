eb84c70b1f75af3ec3f3e98ff38108a3fdd4a40b
nihalerdal <nihalerdall@gmail.com>
2024-06-28 16:11:02 -0700

    React props

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,63 @@
 import * as React from "react";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const Search = () => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-  </div>
-);
-
-const List = () => (
+const Search = () => {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+};
+const List = (props) => (
   <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>{" "}
-        <span>{item.author}</span> <span>{item.num_comments}</span>{" "}
-        <span>{item.points}</span>
-      </li>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item}>
     ))}
   </ul>
 );
 
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
+const Item = (props) => (
+<li>
+  <span> 
+    <a href={props.item.ul}>props.item.title</a>
+  </span>
+  <span>{props.item.author}</span>
+  <span>{props.item.num_comments}</span>
+  <span>{props.item.points}</span>
+</li>
+)
 
-    <Search />
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-    <hr />
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
 
-    <List />
-  </div>
-);
+      <Search />
+
+      <hr />
+
+      <List list={stories} />
+    </div>
+  );
+};
 
 export default App;
