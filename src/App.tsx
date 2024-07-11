1af6e377e5295fb29adccd89f4f18779159a6310
nihalerdal <nihalerdall@gmail.com>
2024-07-11 15:16:15 -0700

    lifting state and filter list via filter()

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,51 @@
 import * as React from "react";
 
-const Search = (props: any) => {
+const App = () => {
+
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   const [searchTerm, setSearchTerm] = React.useState("");
 
-  const handleChange = (event: any) => {
-    setSearchTerm(event.target.value);
-
-    //C: is executed there as callback handler
-    props.onSearch(event);
+  const handleSearch = (event:any) => {
+     setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter((story) => 
+     story.title.includes(searchTerm)
+ );
 
   return (
     <div>
+      <h1>My Hacker Stories</h1>
+      <Search onSearch={handleSearch} />
+      <hr />
+      <List list={searchedStories} />
+    </div>
+  );
+};
+
+const Search = (props: any) => {
+  return (
+    <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
+      <input id="search" type="text" onChange={props.onSearch} />
     </div>
   );
 };
@@ -39,43 +68,5 @@ const Item = (props: any) => (
     <span>{props.item.points}</span>
   </li>
 );
-
-const App = () => {
-  //A: A callback handler gets introduced as event handler
-  const handleSearch = (event: any) => {
-    //D: calls back to the place it was introduced
-    console.log(event.target.value);
-  };
-
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-
-      {/*B: is passed as function in props to another component*/}
-      <Search onSearch={handleSearch} />
-      <hr />
-      <List list={stories} />
-    </div>
-  );
-};
 
 export default App;
