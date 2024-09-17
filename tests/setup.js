b8ed661a3a76e0c9be709540bbde2b8181ff722e
nihalerdal <nihalerdall@gmail.com>
2024-09-16 19:06:31 -0700

    Unit test for components

--- tests/setup.js
+++ tests/setup.js
@@ -1,10 +1,16 @@
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
-import matchers from "@testing-library/jest-dom/matchers";
 import { expect } from "vitest";
-// extends Vitest's expect method with methods from react-testing-library
+import * as matchers from "@testing-library/jest-dom/matchers";
+
+// Extend Vitest's expect method with a specific matcher
 expect.extend(matchers);
-// runs a cleanup after each test case (e.g. clearing jsdom)
+
 afterEach(() => {
   cleanup();
 });
+
+
+
+
+
